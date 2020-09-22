ee11b59a72093926184869b3df6a86c035a7e200
vikithespider@gmail.com <vikrambalaji2k@gmail.com>
2020-09-23 01:38:12 +0530

    added user Dashboard backend

--- common/auth.js
+++ common/auth.js
@@ -1,3 +1,4 @@
+//Used to create the authentication middleware used in users.js to show the dashboard contents
 var jwt=require('jsonwebtoken');
 var authenticate=function(req,res,next){
     if(req.headers.authenticate){
@@ -6,7 +7,7 @@ var authenticate=function(req,res,next){
                 res.json("Token invalid");
             }
             else{
-                next();
+                next();  //next() moves from the middleware to the callback function
             }
         });
     }
--- routes/users.js
+++ routes/users.js
@@ -8,10 +8,12 @@ const app = require('../app');
 const saltRounds=10;
 var url="mongodb://localhost:27017";
 
-router.get('/dashboard',authenticate,function(req,res,next){
-     res.json({
-         message:"protected route"
-     })
+router.get('/dashboard',authenticate,async function(req,res,next){
+     let client=await mongoClient.connect(url);
+     let db=client.db('urlShortener');
+     let userDetails=await db.collection('user').findOne({email:req.headers.email});
+     client.close();
+     console.log(userDetails);
 });
 
 module.exports=router;
